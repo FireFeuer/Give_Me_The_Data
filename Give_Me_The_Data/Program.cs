02ea7bad42f4fdde574e3ab8d0a0d5979897593b
FireFeuer <aidar.bictimirov2017@yandex.ru>
2023-12-02 21:27:32 +0500

    version 0.1

--- Give_Me_The_Data/Program.cs
+++ Give_Me_The_Data/Program.cs
@@ -1,3 +1,30 @@
-﻿
-Console.WriteLine("Hello, World!");
-Console.WriteLine("test_Git");
+﻿using System.Management;
+using System.Net.Sockets;
+
+
+string os = Environment.OSVersion.ToString();
+Console.WriteLine(os.ToString());// просто посмотреть для теста, потом эту строчку надо удалить 
+
+string driveInfo = string.Empty;
+foreach (DriveInfo drive in DriveInfo.GetDrives())
+{
+    if (drive.IsReady)
+    {
+        driveInfo += $"Диск {drive.Name}: Свободное место - {drive.AvailableFreeSpace}\n";
+        
+    }
+}
+Console.WriteLine(driveInfo); // просто посмотреть для теста, потом эту строчку надо удалить 
+
+
+string data = $"Операционная система: {os}nСвободное место на диске:n{driveInfo}"; // подготавливаем наши данные в переменную
+
+
+//TcpClient client = new TcpClient("IP_адрес_сервера", "порт_сервера"); // подключаемся к серверу и создаём клиента 
+
+// Здесь данные должны отправляться на сервер 
+
+//StreamWriter writer = new StreamWriter(client.GetStream());
+//writer.WriteLine(data);
+//writer.Flush();
+//client.Close();
