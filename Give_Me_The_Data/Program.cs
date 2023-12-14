db92c64cd6899983a4c9456a38fb4a0bfe3e6ac4
ShemenV <demonethdr@gmail.com>
2023-12-14 18:37:57 +0500

    version 1.1 раскидал по методам

--- Give_Me_The_Data/Program.cs
+++ Give_Me_The_Data/Program.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Win32;
-using System.Net;
+﻿using System.Net;
 using System.Net.Sockets;
-using System.Reflection;
 using System.Text;
 using System.Text.Json;
 using System.Text.Unicode;
@@ -10,29 +8,108 @@ using System.Text.Unicode;
 // код конфигурационного файла config.env
 partial class Program
 {
-
     static async Task Main(string[] args)
     {
-        string path = Directory.GetCurrentDirectory();
-
-        string pathToEnvFile = Path.Combine(path, "config.env");
         string ip = "";
         int port = 0;
-        string autoload = "";
+        string configFilePath = await CreateConfigFile();
 
+        Dictionary<string, string> network = await ReadConfigFile(configFilePath);
+
+        ip = network["ip"];
+        try
+        {
+            port = int.Parse(network["port"]);
+        }
+        catch
+        {
+            Console.WriteLine($"Используемый порт ({network["port"]}) имеет не верный формат, пожалуйста изменить его в файле config.env и перезапустите программу");
+        }
+
+
+        IPAddress ipAddress = IPAddress.None;
+        try
+        {
+            ipAddress = IPAddress.Parse(ip);
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine($"Используемый IP-Адрес ({ip}) имеет не верный формат, пожалуйста изменить его в файле config.env и перезапустите программу");
+            Console.ReadKey();
+            return;
+        }
+
+
+        TcpListener server = new TcpListener(ipAddress, port);
+        try
+        {
+            server.Start();
+        }
+        catch (SocketException ex)
+        {
+            Console.WriteLine($"Используемый порт ({port}) занят, пожалуйста изменить его в файле config.env и перезапустите программу");
+            Console.ReadKey();
+            return;
+        }
+
+
+        Console.WriteLine($"" +
+            $"Сервер запущен\n" +
+            $"Ip - {ip}\n" +
+            $"Порт - {port}\n" +
+            "Чтобы изменить эти параметры перейдите в файл config.env");
+
+
+        while (true)
+        {
+            List<Dictionary<string, string>> dictitionariesList = await GetClientData(server);
+            foreach (Dictionary<string, string> dict in dictitionariesList)
+            {
+                await SaveClientData(dict);
+            }
+        }
+    }
+
+    public static async Task<string> GetLocalIPAddress()
+    {
+        var host = await Dns.GetHostEntryAsync(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                Console.WriteLine(ip.ToString());
+                return ip.ToString();
+            }
+        }
+        throw new Exception("No network adapters with an IPv4 address in the system!");
+    }
+
+    private async static Task<string> CreateConfigFile()
+    {
+        string path = Directory.GetCurrentDirectory();
+        string pathToEnvFile = Path.Combine(path, "config.env");
         if (!File.Exists(pathToEnvFile))
         {
-            await File.WriteAllTextAsync(pathToEnvFile, $"IP={GetLocalIPAddress()}\r\nPORT=3333\nAUTOLOAD=TRUE");
+            await File.WriteAllTextAsync(pathToEnvFile, $"IP={await GetLocalIPAddress()}\r\nPORT=3333");
         }
-        using (var streamReader = new StreamReader(pathToEnvFile))
+        return pathToEnvFile;
+    }
+
+    private async static Task<Dictionary<string, string>> ReadConfigFile(string configFilePath)
+    {
+        using (var streamReader = new StreamReader(configFilePath))
         {
             string fileContents = await streamReader.ReadToEndAsync();
             string[] lines = fileContents.Split('\n');
+            Dictionary<string, string> network = new Dictionary<string, string>(){
+                { "ip", "-" },
+                {"port","-" }
+            };
 
             foreach (string line in lines)
             {
                 string[] keyValue = line.Split('=');
-                if (keyValue.Length == 3 || keyValue.Length == 2)
+                if (keyValue.Length == 2)
                 {
                     string key_env = keyValue[0].Trim();
                     string value = keyValue[1].Trim();
@@ -40,160 +117,88 @@ partial class Program
                     switch (key_env)
                     {
                         case "IP":
-                            ip = value;
+                            network["ip"] = value;
                             break;
                         case "PORT":
-                            try
-                            {
-                                port = int.Parse(value);
-                            }
-                            catch
-                            {
-                                Console.WriteLine($"Используемый порт ({value}) имеет не верный формат, пожалуйста изменить его в файле config.env и перезапустите программу");
-                                return;
-                            }
-
-                            break;
-                        case "AUTOLOAD":
-                            autoload = value;
+                            network["port"] = value;
                             break;
                     }
                 }
+
             }
-
-            Console.WriteLine(autoload);
-            if (autoload == "TRUE")
-            {
-                const string applicationName = "PCInfo";
-                const string pathRegistryKeyStartup =
-                            "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
-
-                using (RegistryKey registryKeyStartup = Registry.CurrentUser.OpenSubKey(pathRegistryKeyStartup, true))
-                {
-                    registryKeyStartup.SetValue(
-                        applicationName,
-                        string.Format("\"{0}\"", Assembly.GetExecutingAssembly().Location));
-                }
-            }
-
-            IPAddress ipAddress = IPAddress.None;
-            try
-            {
-                ipAddress = IPAddress.Parse(ip);
-            }
-            catch (FormatException ex)
-            {
-                Console.WriteLine($"Используемый IP-Адрес ({ip}) имеет не верный формат, пожалуйста изменить его в файле config.env и перезапустите программу");
-                Console.ReadKey();
-                return;
-            }
-
-            TcpListener server = new TcpListener(ipAddress, port);
-
-            try
-            {
-                server.Start();
-            }
-            catch (SocketException ex)
-            {
-                Console.WriteLine($"Используемый порт ({port}) занят, пожалуйста изменить его в файле config.env и перезапустите программу");
-                Console.ReadKey();
-                return;
-            }
-
-
-            Console.WriteLine($"" +
-                $"Сервер запущен\n" +
-                $"Ip - {ip}\n" +
-                $"Порт - {port}\n" +
-                "Чтобы изменить эти параметры перейдите в файл config.env");
-
-            while (true)
-            {
-                TcpClient client = await server.AcceptTcpClientAsync(); // Принимаем подключение от клиента
-
-                // Читаем данные от клиента
-                StreamReader reader = new StreamReader(client.GetStream());
-                string data = "";
-                try
-                {
-                    data = await reader.ReadToEndAsync();
-                }
-                catch
-                {
-                    Console.WriteLine("Поизощла ошибка на стороне отправителя");
-                }
-
-
-
-                // Создаем настройки для десириализации
-                var options = new JsonSerializerOptions
-                {
-                    Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(UnicodeRanges.All),
-                    WriteIndented = true
-                };
-
-
-                data = data.Remove(data.LastIndexOf(","), 1);
-                Console.WriteLine(data);
-
-
-                byte[] byteArray = Encoding.UTF8.GetBytes(data);
-                MemoryStream stream = new MemoryStream(byteArray);
-
-                // Десиарилизируем полученную строку в словарь 
-                List<Dictionary<string, string>> dictitionariesList = await JsonSerializer.DeserializeAsync<List<Dictionary<string, string>>>(stream, options);
-                client.Close();
-
-
-                foreach (Dictionary<string, string> dict in dictitionariesList)
-                {
-                    string fullInfo = $"" +
-                       $"{dict["name"]}\n" +
-                       $"{dict["driveInfo"]}\n" +
-                       $"Операционная система - {dict["os"]}\n" +
-                       $"{dict["IsWindowsUpdateNeeded"]}\n\n" +
-                       $"Список установленных программ\n" +
-                       $"{dict["programNames"]}\n\n" +
-                       $"Список ошибок системы\n" +
-                       $"{dict["eventsSystem"]}\n" +
-                       $"Список ошибок приложений\n" +
-                       $"{dict["eventsApplication"]}\n";
-
-                    Console.WriteLine(fullInfo);
-
-                    // Сохраняем данные в текстовый файл
-                    string fileName = $"{dict["name"]} инфо - {dict["time"]}.txt";
-
-                    string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-                    folderPath = Path.Combine(folderPath, "PCInfo");
-                    folderPath = Path.Combine(folderPath, dict["name"]);
-                    string fullpath = Path.Combine(folderPath, fileName);
-
-                    DirectoryInfo dirInfo = new DirectoryInfo(folderPath);
-
-                    if (!dirInfo.Exists)
-                    {
-                        dirInfo.Create();
-                    }
-
-                    await File.WriteAllTextAsync(fullpath, fullInfo);
-                }
-            }
+            return network;
         }
     }
 
-    public static string GetLocalIPAddress()
+    private async static Task<List<Dictionary<string, string>>> GetClientData(TcpListener server)
     {
-        var host = Dns.GetHostEntry(Dns.GetHostName());
-        foreach (var ip in host.AddressList)
+        List<Dictionary<string, string>> clientData = new List<Dictionary<string, string>>();
+        TcpClient client = await server.AcceptTcpClientAsync(); // Принимаем подключение от клиента
+
+        // Читаем данные от клиента
+        StreamReader reader = new StreamReader(client.GetStream());
+        string data = "";
+        try
         {
-            if (ip.AddressFamily == AddressFamily.InterNetwork)
-            {
-                return ip.ToString();
-            }
+            data = await reader.ReadToEndAsync();
         }
-        throw new Exception("No network adapters with an IPv4 address in the system!");
+        catch
+        {
+            Console.WriteLine("Поизощла ошибка на стороне отправителя");
+        }
+
+        // Создаем настройки для десириализации
+        var options = new JsonSerializerOptions
+        {
+            Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(UnicodeRanges.All),
+            WriteIndented = true
+        };
+
+        data = data.Remove(data.LastIndexOf(","), 1);
+        Console.WriteLine(data);
+
+        byte[] byteArray = Encoding.UTF8.GetBytes(data);
+        MemoryStream stream = new MemoryStream(byteArray);
+
+        // Десиарилизируем полученную строку в словарь 
+        clientData = await JsonSerializer.DeserializeAsync<List<Dictionary<string, string>>>(stream, options);
+        client.Close();
+
+        return clientData;
+    }
+
+    private async static Task SaveClientData(Dictionary<string, string> dict)
+    {
+        string fullInfo = $"" +
+                   $"{dict["name"]}\n" +
+                   $"{dict["driveInfo"]}\n" +
+                   $"Операционная система - {dict["os"]}\n" +
+                   $"{dict["IsWindowsUpdateNeeded"]}\n\n" +
+                   $"Список установленных программ\n" +
+                   $"{dict["programNames"]}\n\n" +
+                   $"Список ошибок системы\n" +
+                   $"{dict["eventsSystem"]}\n" +
+                   $"Список ошибок приложений\n" +
+                   $"{dict["eventsApplication"]}\n";
+
+        Console.WriteLine(fullInfo);
+
+        // Сохраняем данные в текстовый файл
+        string fileName = $"{dict["name"]} инфо - {dict["time"]}.txt";
+
+        string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        folderPath = Path.Combine(folderPath, "PCInfo");
+        folderPath = Path.Combine(folderPath, dict["name"]);
+        string fullpath = Path.Combine(folderPath, fileName);
+
+        DirectoryInfo dirInfo = new DirectoryInfo(folderPath);
+
+        if (!dirInfo.Exists)
+        {
+            dirInfo.Create();
+        }
+
+        await File.WriteAllTextAsync(fullpath, fullInfo);
     }
 }
 
